c91a4341ced1534af2a7397e2b2a4289bce30119
levgenix <mr.lobach@gmail.com>
2020-10-10 01:15:54 +0300

    All variants

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,13 +1,8 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Duck5 {
     private final Egg6 egg;
 
-    @Autowired
     public Duck5(Egg6 egg) {
         this.egg = egg;
     }
